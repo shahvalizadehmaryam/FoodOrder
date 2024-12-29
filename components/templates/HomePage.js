6a77d9c01ef253822b6d89646a2d6f47bee072fc
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-12-29 13:03:47 +0330

    Instruction component and style file have added and calll in homepage.

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -2,15 +2,17 @@ import Attributes from "../modules/Attributes";
 import Banner from "../modules/Banner";
 import Companies from "../modules/Companies";
 import Definition from "../modules/Definition";
+import Instruction from "../modules/Instruction";
 import styles from "./Home.module.css";
 
 function HomePage() {
   return (
-    <div className={styles.container}> 
+    <div className={styles.container}>
       <Banner />
       <Attributes />
       <Definition />
       <Companies />
+      <Instruction />
     </div>
   );
 }
