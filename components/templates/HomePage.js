f1651e2c42845ceac67a870862044de3111daf5e
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-12-29 12:35:41 +0330

    Definition component called in homepage.

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -5,7 +5,7 @@ import styles from "./Home.module.css";
 
 function HomePage() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container}> 
       <Banner />
       <Attributes />
       <Definition />
